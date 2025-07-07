42b9f25e536d19a7430f08a1080dfa5482473a63
anhkh04 <phamanhkhoa9803@gmail.com>
2025-07-07 13:49:15 +0700

    upd realtime_clock lib

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -10,8 +10,8 @@ date_t date;
 
 int main()
 {
-//	DHT11_Init();
 	TIM2_Init();
+	DHT11_Init();
 	I2C1_Init();
 	LCD_Init();
 	LCD_ClrScr();
@@ -19,18 +19,17 @@ int main()
 
 	while (1)
 	{
-//		receive_data(data);
-//		if (check_sum(data) == 1)
-//		{
-//			LCD_gotoxy(1, 1);
-//			LCD_print_String("HUMI : %d.%d %cRH  -  TEMP : %d.%d %cC", data[0], data[1], 0x25, data[2], data[3], 0xDF);
-//		}
 		time = get_time();
 		date = get_date();
 		LCD_gotoxy(2, 1);
-		LCD_print_String("%s %d%s, %d  -  %d:%02d:%02d", months[date.month - 1], date.day, date.suffix, date.year, time.hour, time.minute, time.second);
+		LCD_print_String("[Sleepy] %c %s %d%s, %d  -  %d:%02d:%02d", 0xA5, months[date.month - 1], date.day, date.suffix, date.year, time.hour, time.minute, time.second);
+		receive_data(data);
+		if (check_sum(data) == 1)
+		{
+			LCD_gotoxy(1, 1);
+			LCD_print_String("HUMI : %d.%d %cRH  -  TEMP : %d.%d %cC", data[0], data[1], 0x25, data[2], data[3], 0xDF);
+		}
 		scroll_left();
-
 	}
 	return 0;
 }
